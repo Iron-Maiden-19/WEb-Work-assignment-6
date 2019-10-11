71012b1a9b2b66194adbb4df15f960491e10e5c4
Jim Ryan <thorbaby2001@yahoo.com>
2019-10-11 14:47:41 -0500

    LEsson 6 webwork
    DS 710 Lesson 6 webwork

--- lesson6 webworks.R
+++ lesson6 webworks.R
@@ -21,13 +21,48 @@ movies %>%
   gf_boxplot(length ~ 1) %>%gf_smooth(length~year)%>%
 gf_refine(scale_y_log10())
 
-movies <- ed_exp5 <- select(filter(education, Region == 2),c(State,Minor.Population:Education.Expenditures))
-           
+ knownBudget <- movies %>%
+  filter(!is.na(budget))
+ 
+ knownBudget %>% 
+   gf_point(length~year, col =~ log10(budget)) %>%
+   gf_smooth(length ~ year) %>%
+   gf_labs(title = 'Length vs. year',
+           caption = 'Source: IMDB.com') %>%
+   gf_refine(scale_y_log10())
+ 
+movies %>%
+   mutate(genre = case_when(
+     Action == 1 ~ "action",
+     Comedy == 1 ~ "comedy",
+     TRUE ~ "other"
+   ))
+ 
+ movies %>% movies  %>%
+   filter(genre)
+
+ movies  %>% 
+   gf_point(length~genre) %>% gf_smooth(length~genre)
+gf_histogram(~length, fill =~ genre,
+             position = position_dodge(),data=movies )
+
+movies %>%
+   gf_bar(~mpaa)%>%
+   gf_refine(scale_y_log10())
+    
+len_by_mpaa <- movies %>%
+   group_by(mpaa) %>%
+   summarise(avg_len = mean(length)) 
+
+len_by_mpaa %>%
+   mutate(mpaa = reorder(mpaa, avg_len)) %>%
+   gf_col(avg_len ~ mpaa, fill = 'blue')
+
+movies %>%
+   gf_boxplot(length ~ mpaa) %>%
+   gf_refine(scale_y_log10())
+      
+
+   
 
  
-
-
-
-
-
-
